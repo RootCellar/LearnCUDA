beda888d6f5206d2627ad8abdd37bccaf9a52788
RootCellar <RootCellar9877@GMail.com>
2023-02-24 19:22:10 -0900

    graphics scaling, CUDA stuff

--- nbody/nbody.cu
+++ nbody/nbody.cu
@@ -26,6 +26,9 @@
 #include "debug.h"
 #include "physics.h"
 
+#define WIDTH 1600
+#define HEIGHT 800
+
 #define PI 3.14159265359
 
 void DrawCircle(float cx, float cy, float r) {
@@ -33,23 +36,49 @@ void DrawCircle(float cx, float cy, float r) {
 	for(float i = 0; i < 2 * PI ; i += 0.1)
 	{
 		float x = r * cosf(i);
+        x /= WIDTH/HEIGHT;
+
 		float y = r * sinf(i);
 		glVertex2f(x + cx, y + cy);
 	}
 	glEnd();
 }
 
+__device__ void calcDistance(float* distance, struct particle one, struct particle two) {
+    (*distance) = sqrtf( pow(one.x-two.x,2) + pow(one.y-two.y,2) );
+}
+
+__global__ void calcAcceleration(struct particle* particles, struct particle center_of_mass) {
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+
+    particles[j].v_x = 1;
+    particles[j].v_y = 1;
+
+    float distance;
+    calcDistance(&distance, particles[j], center_of_mass);
+
+    particles[j].x += particles[j].v_x;
+    particles[j].y += particles[j].v_y;
+}
+
 int main( int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);    // Use single color buffer and no depth buffer.
-    glutInitWindowSize(1600,800);         // Size of display area, in pixels.
+    glutInitWindowSize(WIDTH,HEIGHT);         // Size of display area, in pixels.
     glutInitWindowPosition(0,0);     // Location of window in screen coordinates.
     glutCreateWindow("N-Particle Simulator"); // Parameter is window title.
 
-    int particle_count = 1000;
+    int particle_count = 128*20;
     struct particle* particles = (struct particle*) malloc(sizeof(particle) * particle_count);
     if(particles == 0) {
         printf("Could not allocate memory for particles!\n");
+        exit(1);
+    }
+
+    struct particle* gpu_particles;
+    cudaMalloc(&gpu_particles, sizeof(struct particle) * particle_count);
+    if(gpu_particles == 0) {
+        printf("Could not allocate GPU memory for particles!\n");
         exit(1);
     }
 
@@ -60,7 +89,9 @@ int main( int argc, char** argv) {
     // Position Particles
     for(int i = 0; i < particle_count; i++) {
         particles[i].x = i % 100;
+        particles[i].x *= 15;
         particles[i].y = i / 100;
+        particles[i].y *= 30;
     }
 
     while(1) {
@@ -69,20 +100,25 @@ int main( int argc, char** argv) {
 
         calculate_center_of_mass(&center_of_mass, particles, particle_count);
 
+        cudaMemcpy(gpu_particles, particles, particle_count * sizeof(struct particle), cudaMemcpyHostToDevice);
+
+        calcAcceleration<<<particle_count/128,128>>>(gpu_particles, center_of_mass);
+
+        cudaMemcpy(particles, gpu_particles, particle_count * sizeof(struct particle), cudaMemcpyDeviceToHost);
 
         // Draw it
 
         glClear(GL_COLOR_BUFFER_BIT);
-        DrawCircle(-0.8, 0.1, 0.01);
+        //DrawCircle(-0.8, 0.1, 0.01);
 
         for(int i = 0; i < particle_count; i++) {
-            scaled_x = particles[i].x / 100;
-            scaled_y = particles[i].y / 100;
-            DrawCircle(scaled_x, scaled_y, 0.005);
+            scaled_x = (particles[i].x - WIDTH/2) / (WIDTH/2);
+            scaled_y = (particles[i].y - HEIGHT/2) / (HEIGHT/2);
+            DrawCircle(scaled_x, scaled_y, 0.0025);
         }
 
         glutSwapBuffers();
 
-        sleep(1000);
+        usleep(1000);
     }
 }
