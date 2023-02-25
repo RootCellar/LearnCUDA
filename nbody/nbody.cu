c4de2193da8d1c518d04a80959157d3046e34661
RootCellar <RootCellar9877@GMail.com>
2023-02-24 18:18:52 -0900

    drawing first "circle"

--- nbody/nbody.cu
+++ nbody/nbody.cu
@@ -26,6 +26,19 @@
 #include "debug.h"
 #include "physics.h"
 
+#define PI 3.14159265359
+
+void DrawCircle(float cx, float cy, float r) {
+	glBegin(GL_LINE_LOOP);
+	for(float i = 0; i < 2 * PI ; i += 0.1)
+	{
+		float x = r * cosf(i);
+		float y = r * sinf(i);
+		glVertex2f(x + cx, y + cy);
+	}
+	glEnd();
+}
+
 int main( int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);    // Use single color buffer and no depth buffer.
@@ -33,13 +46,26 @@ int main( int argc, char** argv) {
     glutInitWindowPosition(0,0);     // Location of window in screen coordinates.
     glutCreateWindow("N-Particle Simulator"); // Parameter is window title.
 
-    struct particle* particles = (struct particle*) malloc(sizeof(particle) * 1000);
+    int particle_count = 1000;
+    struct particle* particles = (struct particle*) malloc(sizeof(particle) * particle_count);
     if(particles == 0) {
         printf("Could not allocate memory for particles!\n");
         exit(1);
     }
 
+    struct particle center_of_mass;
+
     while(1) {
+
+        // Do the physics
+
+        calculate_center_of_mass(&center_of_mass, particles, particle_count);
+
+
+        // Draw it
+        glClear(GL_COLOR_BUFFER_BIT);
+        DrawCircle(-0.8, 0.1, 0.01);
+        glutSwapBuffers();
 
         sleep(1000);
     }
