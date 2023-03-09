2517a3c440124e124f24bfa5571679f081fe34d0
RootCellar <RootCellar9877@GMail.com>
2023-03-09 10:38:39 -0900

    properly benchmark floats, add benchmarking ints (not working yet) and doubles (working)

--- benchmark/benchmark.cu
+++ benchmark/benchmark.cu
@@ -57,17 +57,37 @@ __global__ void benchFloats(float* floats) {
 
     int j = blockIdx.x * blockDim.x + threadIdx.x;
 
-    for(int i = 0; i < 1000; i++) {
-        floats[j] *= 6.7;
-        floats[j] *= 6.7;
-        floats[j] *= 6.7;
-        floats[j] *= 6.7;
-        floats[j] *= 6.7;
-        floats[j] *= 6.7;
-        floats[j] *= 6.7;
-        floats[j] *= 6.7;
-        floats[j] *= 6.7;
-        floats[j] *= 6.7;
+    for(float i = 0; i < 1000000; i+=1) {
+        float x = 5.5 * i * i;
+        float y = 5.5;
+        float z = x * y;
+        floats[j] = z;
+    }
+
+}
+
+__global__ void benchInts(int* ints) {
+
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+
+    for(int i = 0; i < 1000000; i+=1) {
+        int x = 5 * i * i;
+        int y = 5;
+        int z = x * y;
+        ints[j] = z;
+    }
+
+}
+
+__global__ void benchDoubles(double* doubles) {
+
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+
+    for(double i = 0; i < 1000000; i+=1) {
+        double x = 5 * i * i;
+        double y = 5;
+        double z = x * y;
+        doubles[j] = z;
     }
 
 }
@@ -95,24 +115,30 @@ int main(int argc, char** argv) {
     int runCount;
     float seconds;
 
-    float* floats = (float*) malloc(sizeof(float) * BLOCKS);
+    // For both mallocs, we allocate twice the space needed for an array of floats
+    // because we will also benchmark doubles, which are twice the size
+
+    float* floats = (float*) malloc(sizeof(float) * BLOCKS * 2);
     float* gpu_floats;
 
-    cudaMalloc(&gpu_floats, sizeof(float) * BLOCKS);
+    cudaMalloc(&gpu_floats, sizeof(float) * BLOCKS * 2);
 
     if(gpu_floats == 0 || floats == 0) {
         perror("Could not allocate space for floats\n");
         exit(1);
     }
 
+    /*
     for(int i = 0; i < BLOCKS; i++) {
         floats[i] = (float) i;
     }
+    */
 
     runCount = 0;
     start_time = clock();
     while( (time_now = clock() ) - start_time < CLOCKS_PER_SEC * SECONDS_PER_RUN) {
         cudaMemcpy(gpu_floats, floats, sizeof(float) * BLOCKS, cudaMemcpyHostToDevice);
+        cudaDeviceSynchronize();
         runCount++;
     }
 
@@ -136,6 +162,14 @@ int main(int argc, char** argv) {
     for(int i = 0; i < 10; i++) {
         //cudaMemcpy(gpu_floats, floats, sizeof(float) * BLOCKS, cudaMemcpyHostToDevice);
         BENCHMARK(benchFloats, gpu_floats, "benchFloats");
+    }
+
+    for(int i = 0; i < 10; i++) {
+        BENCHMARK(benchInts, (int*) gpu_floats, "benchInts");
+    }
+
+    for(int i = 0; i < 10; i++) {
+        BENCHMARK(benchDoubles, (double*) gpu_floats, "benchDoubles");
     }
 
 }
