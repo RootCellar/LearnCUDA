c8fb5e0063243acd4cf9f6ca18344a13babcc00e
RootCellar <RootCellar9877@GMail.com>
2023-03-09 15:12:52 -0900

    comments

--- benchmark/benchmark.cu
+++ benchmark/benchmark.cu
@@ -30,22 +30,32 @@
  *
 
     do {
+        // How many times the function was run in the elapsed time
         int runCount = 0;
+
+        // For keeping track of how long we've spent running this
         clock_t start_time = clock();
         clock_t time_now;
+
+        // Call the function over and over until at least one second has elapsed
         while( (time_now = clock() ) - start_time < CLOCKS_PER_SEC * SECONDS_PER_RUN) {
-            function<<<BLOCKS/128, 128>>>(values);
-            cudaDeviceSynchronize();
+
+            function<<<BLOCKS/128, 128>>>(values); // Run the function we are benchmarking
+
+            cudaDeviceSynchronize(); // Wait for the device to finish that before we move on
+
             runCount++;
+
         }
 
+        // Calculate how long this actually took and print the results
         float seconds = (float) (time_now - start_time) / CLOCKS_PER_SEC;
         debug_printf("bench: %d over %f seconds\n", runCount, seconds);
     } while(0)
 
  *
  * This makes benchmarking much easier. The do-while loop surrounds the whole thing just make sure
- * it is in it's own code block and won't behave weirdly inside of other blocks (for loops, etc)
+ * it is in its own code block and won't behave weirdly inside of other blocks (for loops, etc)
  *
 */
 
@@ -53,6 +63,23 @@
     while( ( time_now = clock() ) - start_time < CLOCKS_PER_SEC * SECONDS_PER_RUN) { x<<<BLOCKS/128, 128>>>(vals); cudaDeviceSynchronize(); runCount++; }\
     float seconds = (float) (time_now - start_time) / CLOCKS_PER_SEC; debug_printf(name ": %d over %f seconds\n", runCount, seconds); } while(0)
 
+
+/*
+ * Performs 1 million iterations per thread, 4 floating point operations each iteration (theoretically)
+ * and of course checks to see if the loop is done yet each iteration
+ *
+ * On my GTX 1660 Ti, this runs 23 times in 1.035 seconds on average.
+ * (23/1.035) times per second * 1,000,000 iterations * 4 operations per iteration * 128*1000 threads
+ * = 11,377,777,777,778 FLOPS (theoretically)
+ * = 11.378 TFLOPS (theoretically)
+ * 
+ * The interesting thing to note is that websites such as techpowerup which calculate theoretical TFLOPS
+ * come up with only about 4.9 TFLOPS.
+ * 
+ * This indicates significant compiler optimization. Also, my GPU actually operates at a higher clock rate than
+ * techpowerup claims.
+ *
+*/
 __global__ void benchFloats(float* floats) {
 
     int j = blockIdx.x * blockDim.x + threadIdx.x;
@@ -79,6 +106,21 @@ __global__ void benchInts(int* ints) {
 
 }
 
+/*
+ * Performs 1 million iterations per thread, 4 double-precision floating point operations each iteration (theoretically)
+ * and of course checks to see if the loop is done yet each iteration
+ *
+ * On my GTX 1660 Ti, this runs 1 time in 1.7 seconds on average.
+ * (1/1.7) times per second * 1,000,000 iterations * 4 operations per iteration * 128*1000 threads
+ * = 301,176,470,588 FLOPS (theoretically)
+ * = 301.176 GFLOPS (theoretically)
+ * 
+ * Techpowerup claims a GTX 1660 Ti mobile can do 152 GFLOPS
+ *
+ * This is likely compiler optimization at work again.
+ * However, I still notice that this is WAAAY slower than regular single-precision floating-point math operations.
+ *
+*/
 __global__ void benchDoubles(double* doubles) {
 
     int j = blockIdx.x * blockDim.x + threadIdx.x;
