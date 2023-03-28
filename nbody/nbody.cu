5b252582112cd96966dadf10c714f63510f7be48
RootCellar <RootCellar9877@gmail.com>
2023-03-28 14:16:03 -0800

    formatting

--- nbody/nbody.cu
+++ nbody/nbody.cu
@@ -52,12 +52,17 @@ float calcSpeed(struct particle* p) {
 
 void DrawParticle(struct particle* p) {
     glBegin(GL_POINTS);
+
     float speed = calcSpeed(p);
     float redness = speed*40; // TODO: find a better way to calculate this
+    
     glColor3f(redness, 1-redness, 1-redness);
+
     float scaled_x = (p->x - WIDTH/2) / (WIDTH/2);
     float scaled_y = (p->y - HEIGHT/2) / (HEIGHT/2);
+
     glVertex2f(scaled_x, scaled_y);
+
     glEnd();
 }
 
